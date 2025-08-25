9f9624a4f8f2404c1cc5bdacbc68d906a3d94f5b
Maksymilia Gunerka <maksafk@gmail.com>
2025-08-25 11:29:10 +0100

    change for deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly REACT_APP_SUPABASE_KEY: string
-    readonly REACT_APP_SUPABASE_URL: string
+    readonly VITE_SUPABASE_KEY: string
+    readonly VITE_SUPABASE_URL: string
     // more env variables...
   }
   
